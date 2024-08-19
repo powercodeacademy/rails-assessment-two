4fd3e4e914108ae87e20c49cfc4bc757a155c67b
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-08-19 12:36:41 -0400

    Add show for vendor_sweets comment

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -6,8 +6,8 @@ class VendorsController < ApplicationController
 
   def show
     @vendor = Vendor.find(params[:id])
+    @vendor_sweet = @vendor.vendor_sweets.build
   rescue ActiveRecord::RecordNotFound
     redirect_to vendors_path, alert: "Vendor not found"
   end
-
 end
