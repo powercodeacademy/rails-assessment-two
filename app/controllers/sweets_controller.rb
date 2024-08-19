ce5d355a8d9aab978f55623e63468c44e3ac1ddb
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-08-19 10:51:28 -0400

    Create link to sweet show page

--- app/controllers/sweets_controller.rb
+++ app/controllers/sweets_controller.rb
@@ -2,4 +2,11 @@ class SweetsController < ApplicationController
   def index
     @sweets = Sweet.all
   end
+
+  def show
+    @sweet = Sweet.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to vendors_path, alert: "Sweet not found"
+  end
+
 end
