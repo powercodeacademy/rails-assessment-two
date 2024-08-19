ecc80b7e231cb4c2e01e3e6cbd5cd9bd1fa20a82
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-08-19 10:43:07 -0400

    Create vendor show route and view

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -4,4 +4,10 @@ class VendorsController < ApplicationController
     @vendors = Vendor.all
   end
 
+  def show
+    @vendor = Vendor.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to vendors_path, alert: "Vendor not found"
+  end
+
 end
