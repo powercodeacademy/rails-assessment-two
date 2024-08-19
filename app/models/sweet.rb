887e81e0b700017f773ca68f9545e095846730fc
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-08-19 10:48:41 -0400

    Create link to vendor show page

--- app/models/sweet.rb
+++ app/models/sweet.rb
@@ -1,3 +1,5 @@
 class Sweet < ApplicationRecord
+  has_many :vendor_sweets
+  has_many :vendors, through: :vendor_sweets
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/sweets', to: "sweets#index", as: "sweets"
-  get '/sweets', to: "vendors#index", as: "vendors"
+  get '/vendors', to: "vendors#index", as: "vendors"
 
   get '/vendors/:id', to: 'vendors#show', as: "vendor"
   get '/sweets/:id', to: 'sweets#show', as: "sweet"
