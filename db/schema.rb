1bac770c41f075fa08051020094113ed252ad629
Nihar Patel <niharpatel52@gmail.com>
2025-08-25 11:33:01 -0400

    Add comments column to sweets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_25_131529) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_25_152931) do
   create_table "sweets", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
+    t.string "comment"
   end
 
   create_table "vendor_sweets", force: :cascade do |t|
