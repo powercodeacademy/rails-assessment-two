3a2df9226eaee2d333460b24efdb154e53d51d07
Nihar Patel <niharpatel52@gmail.com>
2025-08-25 10:00:15 -0400

    Update seeds and update vendor#show to display sweets and sweets#show display vendor, and links

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+VendorSweet.destroy_all
 Vendor.destroy_all
 Sweet.destroy_all
 
@@ -27,10 +28,29 @@ sweets = [
   "Peanut Butter Icecream Cake",
 ]
 
+vendor_sweets = [
+  { vendor_id: 1, sweet_id: 1 },
+  { vendor_id: 1, sweet_id: 5 },
+  { vendor_id: 2, sweet_id: 2 },
+  { vendor_id: 2, sweet_id: 3 },
+  { vendor_id: 3, sweet_id: 6 },
+  { vendor_id: 3, sweet_id: 1 },
+  { vendor_id: 4, sweet_id: 4 },
+  { vendor_id: 4, sweet_id: 5 },
+  { vendor_id: 5, sweet_id: 2 },
+  { vendor_id: 5, sweet_id: 6 },
+  { vendor_id: 6, sweet_id: 3 },
+  { vendor_id: 6, sweet_id: 4 }
+]
+
 vendors.each do |vendor|
   Vendor.create(name: vendor)
 end
 
 sweets.each do |sweet|
   Sweet.create(name: sweet)
+end
+
+vendor_sweets.each do |vs|
+  VendorSweet.create!(vendor_id: vs[:vendor_id], sweet_id: vs[:sweet_id])
 end
