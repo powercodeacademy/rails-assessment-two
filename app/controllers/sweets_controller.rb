60f86fc49eb412cc92e310ce99cd99f612ecb30c
Nihar Patel <niharpatel52@gmail.com>
2025-08-25 09:14:07 -0400

    Add links to each vendor/sweet on index page and create vendor#show and sweet#show pages

--- app/controllers/sweets_controller.rb
+++ app/controllers/sweets_controller.rb
@@ -1,5 +1,14 @@
 class SweetsController < ApplicationController
+  before_action :set_sweet, only: [:show, :edit, :update, :destroy]
   def index
     @sweets = Sweet.all
+  end
+
+  def show
+  end
+
+private
+  def set_sweet
+      @sweet = Sweet.find(params[:id])
   end
 end
--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,7 +1,14 @@
 class VendorsController < ApplicationController
-
+  before_action :set_vendor, only: [:show, :edit, :update, :destroy]
   def index
     @vendors = Vendor.all
   end
 
+  def show
+  end
+
+private
+  def set_vendor
+      @vendor = Vendor.find(params[:id])
+  end
 end
