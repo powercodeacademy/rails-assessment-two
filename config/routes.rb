cf248733144cf0af385a650b70f67ad5d6459e31
Emily Kanarek <emilyakanarek@gmail.com>
2025-08-26 08:55:00 -0400

    Set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/sweets', to: "sweets#index", as: "sweets"
-  get '/vendors', to: "vendors#index", as: "vendors"
-
+  resources :sweets, only: [:index, :show]
+  resources :vendors, only: [:index, :show]
+  resources :vendor_sweets, only: [:new, :create]
 end
