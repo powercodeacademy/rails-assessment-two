9f4bece7702637d2d3eae4a971ddbcfaf4802f67
elizabethwilby <elizabethwilby@Elizabeths-MacBook-Pro.local>
2026-08-18 09:42:53 -0400

    finished lab

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/sweets', to: "sweets#index", as: "sweets"
-  get '/vendors', to: "vendors#index", as: "vendors"
-
+  resources :vendors do
+    resources :vendor_sweets, only: [:create]
+  end
+  resources :sweets, only: [:index, :show]
 end
